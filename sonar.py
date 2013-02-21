3bc455847dcfa31f80b042961cf0f6b99612d5be
c0nrad <sclarsen@mtu.edu>
2013-02-20 20:14:09 -0500

    Changed sonar to a class

--- sonar.py
+++ sonar.py
@@ -1,47 +1,54 @@
 #!/usr/bin/python
-
 import time
 import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-
 
 GPIO_TRIGGER = 23
 GPIO_ECHO = 24
 
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
-GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+class Sonar:
+  def __init__(self, trigPin = GPIO_TRIGGER, echoPin = GPIO_ECHO):
 
-GPIO.output(GPIO_TRIGGER, False)
+    GPIO.setmode(GPIO.BCM)
+    self.mTriggerPin = trigPin
+    self.mEchoPin = echoPin
 
-# Allow module to settle
+    GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
+    GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
 
-while True:
-  time.sleep(0.1)
+    GPIO.output(GPIO_TRIGGER, False)
 
-# Send 10us pulse to trigger
-  GPIO.output(GPIO_TRIGGER, True)
-  time.sleep(0.00001)
-  GPIO.output(GPIO_TRIGGER, False)
-  start = time.time()
-  
-  while GPIO.input(GPIO_ECHO)==0:
+  def readContinuous(self):
+    
+    while True:
+      time.sleep(0.1)
+      value = self.readValue()
+      print value
+
+  def readValue(self):
+    
+    # Send 10us pulse to trigger
+    GPIO.output(self.mTriggerPin, True)
+    time.sleep(0.00001)
+    GPIO.output(self.mTriggerPin, False)
     start = time.time()
-
-  while GPIO.input(GPIO_ECHO)==1:
-    stop = time.time()
-
-  # Calculate pulse length
-  elapsed = stop-start
   
-  # Distance pulse travelled in that time is time
-  # multiplied by the speed of sound (cm/s)
-  distance = elapsed * 34000
+    while GPIO.input(self.mEchoPin)==0:
+      start = time.time()
+        
+    while GPIO.input(self.mEchoPin)==1:
+      stop = time.time()
 
-  # That was the distance there and back so halve the value
-  distance = distance / 2
+    # Calculate pulse length
+    elapsed = stop-start
+  
+    # Distance pulse travelled in that time is time
+    # multiplied by the speed of sound (cm/s)
+    distance = elapsed * 34000
 
-  print "Distance : %.1f" % distance
+    # That was the distance there and back so halve the value
+    distance = distance / 2
 
-# Reset GPIO settings
-GPIO.cleanup()
+    return distance
+
+  def cleanup(self):
+    GPIO.cleanup()
